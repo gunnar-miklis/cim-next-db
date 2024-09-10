0770749fa2fa6f255581c35a5ab1fde82f9ba1fd
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-10 18:51:04 +0200

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+import styles from '@/src/styles/app.module.css';
+
+export default function NotFound() {
+  return (
+    <main className={styles.main}>
+      <h1>NotFound</h1>
+      <p>Could not find requested resource</p>
+    </main>
+  );
+}
+
+export const metadata: Metadata = { title: 'Not Found' };
